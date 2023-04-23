817b5a4d6a7f1287e1ff79666668d36330cc0223
Johannes Maendle <johannes@studysmarter.de>
2023-04-23 21:33:25 +0200

    feat: add more keywords, add onboarding page

--- js/background.js
+++ js/background.js
@@ -1,3 +1,15 @@
-chrome.runtime.onInstalled.addListener(function() {
-    window.alert("Successfully installed extension!");
+await chrome.storage.managed.clear();
+
+chrome.runtime.onInstalled.addListener(({reason}) => {
+    if (reason === 'install') {
+        void chrome.tabs.create({
+            url: "onboarding.html"
+        });
+    }
+});
+
+chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
+    if (tab) {
+        await chrome.tabs.sendMessage(tab.id, {currentTab: tab});
+    }
 });
--- js/content.js
+++ js/content.js
@@ -1,41 +1,107 @@
-const SEARCH_KEYWORDS = [
+const GERMAN_KEYWORDS = [
     'accept',
     'akzeptieren',
     'zustimmen',
     'alles zustimmen',
-    'accept all',
     'alle akzeptieren',
     'AKZEPTIEREN UND WEITER',
-    'accept all cookies',
     'Annehmen',
+    'Einverstanden'
+];
+
+const ENGLISH_KEYWORDS = [
+    'accept all',
+    'accept all cookies',
+    'accept and continue',
+    'allow',
+    'allow all cookies'
 ]
 
-window.onload = function() {
-    console.log("Hello World New!");
+const SEARCH_KEYWORDS = [...GERMAN_KEYWORDS, ...ENGLISH_KEYWORDS];
 
-    setTimeout(() => {
+let isInitialized = false;
+
+chrome.runtime.onMessage.addListener(() => {
+    if (isInitialized) {
+        chrome.storage.sync.set({'currentHostname': location.hostname}, () => {
+            console.info('💾 Host name saved.', location.hostname);
+        });
+    } else {
+        console.info('Not yet initialized. Host name not saved.')
+    }
+})
+
+window.onload = function() {
+    chrome.storage.sync.get(['currentHostname'], (items) => {
+        if (items['currentHostname'] === location.hostname) {
+            console.info('✅ Same hostname, skip Cookie check.', location.hostname);
+            isInitialized = true;
+
+            return;
+        }
+
+        console.log('🔎 Searching for Cookie Banners...');
         searchForCookieConsentElements();
-    }, 1000)
+        isInitialized = true;
+    });
 }
 
 function searchForCookieConsentElements() {
    SEARCH_KEYWORDS.forEach(keyword => {
-         const elements = getElementsByText(keyword).filter(element => ['button', 'a', 'form'].includes(element.tagName.toLowerCase()));
-         console.log(`Found ${elements.length} elements with keyword ${keyword}`);
-         console.log(elements);
-         elements.forEach(element => {
-              element.click();
-         })
+       let hasElements;
+       let hasClickedOnElements = false;
+
+       const elements = getElementsByText(keyword).filter(element => ['button', 'a', 'form'].includes(element.tagName.toLowerCase()));
+       hasElements = elements.length > 0;
+
+       if (hasElements) {
+           elements.forEach(element => {
+               element.click();
+               hasClickedOnElements = true;
+               console.info('Element clicked: ', element);
+           })
+       }
+
+       if (!hasElements || !hasClickedOnElements) {
+           const iframeDocuments = getAllIframeDocuments();
+
+           if (iframeDocuments.length > 0) {
+               console.info('iFrame found!');
+
+            const elements = iframeDocuments.map(iframeDocument => getElementsByText(keyword, iframeDocument).filter(element => ['button', 'a', 'form'].includes(element.tagName.toLowerCase())));
+
+               if (elements.length > 0) {
+                   elements.forEach(element => {
+                       element.click();
+                       hasClickedOnElements = true;
+                       console.info('iFrame Element clicked: ', element);
+                   })
+               }
+           }
+       }
+
    });
 }
 
-function getElementsByText(text) {
-    const elements = document.getElementsByTagName('*');
+function getElementsByText(text, iframeDocument) {
+    const elements = (iframeDocument ?? document).getElementsByTagName('*');
     const matches = [];
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].textContent.toLocaleLowerCase().includes(text.toLocaleLowerCase())) {
             matches.push(elements[i]);
         }
     }
+
     return matches;
+}
+
+function getAllIframeDocuments() {
+    const iframes = document.querySelectorAll('iframe');
+    let result = []
+
+    if (iframes.length > 0) {
+        result = Array.from(iframes).map(i => i.contentDocument || i.contentWindow.document)
+    }
+
+    return result;
 }
